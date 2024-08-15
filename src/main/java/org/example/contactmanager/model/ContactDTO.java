bf7d4e96f9def745474952d77177b5d84eec80b6
Natalia <kolchenko.nata.yu@gmail.com>
2024-08-15 09:56:54 +0200

    Create DTO model

--- src/main/java/org/example/contactmanager/model/ContactDTO.java
+++ src/main/java/org/example/contactmanager/model/ContactDTO.java
@@ -3,23 +3,18 @@ package org.example.contactmanager.model;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.text.SimpleDateFormat;
-
 @Getter
 @Setter
-public class Contact {
-    private int id;
+public class ContactDTO {
     private String firstName;
     private String lastName;
     private String email;
     private String phone;
 
-    public Contact(String firstName, String lastName, String email, String phone) {
+    public ContactDTO(String firstName, String lastName, String email, String phone) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
         this.phone = phone;
     }
-
 }
-
